84a4d96839204bebbd9af5c7168155bdb093b97f
Nilton-SPTech <nilton.gomes@sptech.school>
2023-03-12 03:49:04 -0300

    Finalizando a home e a responsividade, ainda faltaa arrumar alguns detalhes

--- Web/public/js/parallaxHome.js
+++ Web/public/js/parallaxHome.js
@@ -2,10 +2,34 @@ const pessoaMala = document.getElementById("imgPessoaMala");
 const planta = document.getElementById("imgPlanta");
 const tabela = document.getElementById("imgTabela");
 
-var posicaoPessoaMala = pessoaMala.style.right;
+const bgImgFundo = document.getElementsByClassName("banner");
+const body = document.getElementsByTagName("body");
+const esteira = document.getElementById("esteira");
 
+var valor = 0;
+var mensagem = pessoaMala.style.right
+
+var largura = window.screen.width;
+console.log(largura);
+
+console.log(mensagem + "px");
 window.addEventListener('scroll', function(){
-    console.log(posicaoPessoaMala);
-    let valor = this.window.scrollY;
-    pessoaMala.style.right = (220 + ((valor * 0.1) * -1))  + 'px';
+    valor = this.window.scrollY;
+    console.log(valor);
+    
+    // PESSOA MOVER PARA DIREITA
+    if(largura < 400){
+        pessoaMala.style.right = (10+ ((valor * 0.4) * -1))  + 'px';
+    }
+    else{
+        pessoaMala.style.right = (220 + ((valor * 0.4) * -1))  + 'px';
+    }
+
+    // MALA E PLANTA MOVER PARA ESQUERDA
+    planta.style.right = -70 + (valor * 0.1) + 'px';
+    tabela.style.right = 400 + (valor * 0.2) + 'px';
+
+    if(valor <= 700){
+        esteira.style.bottom = -1000 + (valor * 1.4) + 'px';
+    }
 });
