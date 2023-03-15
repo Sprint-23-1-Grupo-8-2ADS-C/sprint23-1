a3633ac9a8b7c4acf909f0687ff0c20fd08abc84
Marcus Gonçalves <marcus.goncalves@sptech.school>
2023-03-15 15:53:06 -0300

    Rota de login com dados chumbados para teste

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,25 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
 
 export async function routes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const funcionario = await prisma.funcionario.findMany()
-  
-    return funcionario
+  app.post("/login", async (request) => {
+    const emailLogin = "fernandez.renan@gol.com.br";
+    const senhaLogin = "54321";
+    // const login = z.object({
+    //   emailLogin: z.string(),
+    //   senhaLogin: z.string(),
+    // });
+
+    // const { emailLogin, senhaLogin } = login.parse(request.body);
+
+    const selectUsuario = await prisma.funcionario.findMany({
+      where: {
+        email: emailLogin,
+        senha: senhaLogin,
+      }
+    });
+
+    console.log(selectUsuario);
   });
 }
